c1959f32c83aa4bbd8bb3b707981c804a00646d5
mohamed <muridi51@gmail.com>
2021-06-23 15:09:23 +0100

    first commit, after setting up components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(): JSX.Element{
+    return (
+        <h1>Thank you for visiting</h1>
+    )
+}
+
+
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header(): JSX.Element{
+    return (
+        <h1>Welcome to Beyond the Cockpit</h1>
+    )
+}
+
+
+
+export default Header;
