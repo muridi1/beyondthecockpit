bf240486032a79da0d543b56dac7d306fda329ec
Vyoma Gupta <vyomagupta@outlook.com>
2021-06-24 12:22:09 +0100

    running tests 1

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,24 +1,30 @@
 import { title } from "process";
 import BlogPost from "./BlogPost";
 
-const blogArr = [{title:"Meet the Crew", date: "23rd June 2021",  body: "Our first blog post", author: "Mohamed and Vyoma"}]
+const blogArr = [
+  {
+    title: "Meet the Crew",
+    date: "23rd June 2021",
+    body: "Our first blog post",
+    author: "Mohamed and Vyoma",
+  },
+];
 
 function Content(): JSX.Element {
-    return (
-      <main> 
-        {blogArr.map(blogpost=><BlogPost
-        key = {title}
-        title={blogpost.title}
-        date={blogpost.date}
-        //image={blogpost.image}
-        body={blogpost.body}
-        author={blogpost.author}
-        />)}
-
-      </main>
-
-    )
-
+  return (
+    <main>
+      {blogArr.map((blogpost) => (
+        <BlogPost
+          key={title}
+          title={blogpost.title}
+          date={blogpost.date}
+          //image={blogpost.image}
+          body={blogpost.body}
+          author={blogpost.author}
+        />
+      ))}
+    </main>
+  );
 }
 
 export default Content;
